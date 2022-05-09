0a23594417f7d21f71b8cc9cb3bdc7cfcd588409
LuisCasillasA <launchx10359@innovaccion.mx>
2022-05-09 18:32:56 -0500

    ExplorerService test

---
+++ test/services/ExplorerService.test.js
@@ -0,0 +1,19 @@
+const ExplorerService = require('./../../lib/services/ExplorerService')
+
+describe('Unit test for ExplorerService', () =>{
+    test('1. Get all explorers in mission', () =>{
+        const explorer = [{mission: 'node'}]
+        const explorersInNode = ExplorerService.filterByMission(explorer, 'node')
+        expect(explorersInNode.length).toBe(1)
+    });
+    test('2. Get amount explorers in mission', () =>{
+        const explorer = [{mission: 'node'}]
+        const amount = ExplorerService.getAmountOfExplorersByMission(explorer, 'node')
+        expect(amount).toBe(1)
+    })
+    test('3. Ger explorers usernames in mission',() =>{
+        const explorer = [{mission: 'node', githubUsername: 'Luis'}]
+        const usernames = ExplorerService.getExplorersUsernamesByMission(explorer, 'node')
+        expect(usernames).toContain('Luis')
+    })
+})
