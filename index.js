bf3203162c988b840675191ad73f8feba9d986df
LuisCasillasA <launchx10359@innovaccion.mx>
2022-05-09 17:22:27 -0500

    Using ExplorerService

--- index.js
+++ index.js
@@ -2,4 +2,9 @@
 const Reader = require('./lib/utils/Reader');
 const explorers = Reader.readJsonFile('./explorers.json');
 
-console.log(explorers)
+//Using ExplorerService on the list
+const ExplorerService = require('./lib/services/ExplorerService')
+
+ExplorerService.filterByMission(explorers, "node");
+ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+ExplorerService.getExplorersUsernamesByMission(explorers, "node");
