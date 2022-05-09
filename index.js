d344a7b62313036a544abf0cb36d9834de08acc4
LuisCasillasA <launchx10359@innovaccion.mx>
2022-05-09 17:34:45 -0500

    Fizz Buzz service

--- index.js
+++ index.js
@@ -8,3 +8,18 @@ const ExplorerService = require('./lib/services/ExplorerService')
 ExplorerService.filterByMission(explorers, "node");
 ExplorerService.getAmountOfExplorersByMission(explorers, "node");
 ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+
+//Validations
+const FizzBuzzService = require('./lib/services/FizzBuzzService')
+
+const explorer1 = {name: "Explorer1", score: 1}
+FizzBuzzService.applyValidationInExplorer(explorer1) // {name: "Explorer1", score: 1, trick: 1} 
+
+const explorer3 = {name: "Explorer3", score: 3}
+console.log(FizzBuzzService.applyValidationInExplorer(explorer3)) // {name: "Explorer3", score: 3, trick: "FIZZ"}
+
+const explorer5 = {name: "Explorer5", score: 5}
+console.log(FizzBuzzService.applyValidationInExplorer(explorer5)) // {name: "Explorer5", score: 5, trick: "BUZZ"}
+
+const explorer15 = {name: "Explorer15", score: 15}
+console.log(FizzBuzzService.applyValidationInExplorer(explorer15)) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
