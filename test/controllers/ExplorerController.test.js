c50c6a16210a107ec6a72a351226fc6b2a9d9801
LuisCasillasA <launchx10359@innovaccion.mx>
2022-05-09 21:43:23 -0500

    Linter code

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -2,8 +2,8 @@ const ExplorerController = require("./../../lib/controllers/ExplorerController")
 
 describe("Unit test for ExplorerController", () =>{
     test("1. Get all explorers in mission", () =>{
-        const nodeExplorers = ExplorerController.getExplorersByMission('node')
-        expect(nodeExplorers.every((explorer) => explorer.mission === 'node')).toBe(true);
+        const nodeExplorers = ExplorerController.getExplorersByMission("node");
+        expect(nodeExplorers.every((explorer) => explorer.mission === "node")).toBe(true);
     });
     test("2. Get amount explorers in mission", () =>{
         const amount = ExplorerController.getAmountOfExplorersByMission("node");
