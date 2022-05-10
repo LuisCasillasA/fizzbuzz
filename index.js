f3f7c8d4ed75bd0aea61d9de25d0c9962d221891
LuisCasillasA <launchx10359@innovaccion.mx>
2022-05-09 19:46:29 -0500

    structuring code with linter

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 //Read file
-const Reader = require('./lib/utils/Reader');
-const explorers = Reader.readJsonFile('./explorers.json');
+const Reader = require("./lib/utils/Reader");
+const explorers = Reader.readJsonFile("./explorers.json");
 
 //Using ExplorerService on the list
-const ExplorerService = require('./lib/services/ExplorerService')
+const ExplorerService = require("./lib/services/ExplorerService");
 
 ExplorerService.filterByMission(explorers, "node");
 ExplorerService.getAmountOfExplorersByMission(explorers, "node");
 ExplorerService.getExplorersUsernamesByMission(explorers, "node");
 
 //Validations
-const FizzBuzzService = require('./lib/services/FizzBuzzService')
+const FizzBuzzService = require("./lib/services/FizzBuzzService");
 
-const explorer1 = {name: "Explorer1", score: 1}
-FizzBuzzService.applyValidationInExplorer(explorer1) // {name: "Explorer1", score: 1, trick: 1} 
+const explorer1 = {name: "Explorer1", score: 1};
+FizzBuzzService.applyValidationInExplorer(explorer1); // {name: "Explorer1", score: 1, trick: 1} 
 
-const explorer3 = {name: "Explorer3", score: 3}
-FizzBuzzService.applyValidationInExplorer(explorer3) // {name: "Explorer3", score: 3, trick: "FIZZ"}
+const explorer3 = {name: "Explorer3", score: 3};
+FizzBuzzService.applyValidationInExplorer(explorer3); // {name: "Explorer3", score: 3, trick: "FIZZ"}
 
-const explorer5 = {name: "Explorer5", score: 5}
-FizzBuzzService.applyValidationInExplorer(explorer5) // {name: "Explorer5", score: 5, trick: "BUZZ"}
+const explorer5 = {name: "Explorer5", score: 5};
+FizzBuzzService.applyValidationInExplorer(explorer5); // {name: "Explorer5", score: 5, trick: "BUZZ"}
 
-const explorer15 = {name: "Explorer15", score: 15}
-FizzBuzzService.applyValidationInExplorer(explorer15) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
+const explorer15 = {name: "Explorer15", score: 15};
+FizzBuzzService.applyValidationInExplorer(explorer15); // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
